3a42db7e32a628f1084f212722e7886d26cffb04
Tom White <tom@sixdozen.com>
2013-10-28 14:53:00 -0700

    Moved counterfn into cascalog.
    Also cleaned up code so as not to distract from merge proposal.

--- src/wordcount/counter.clj
+++ src/wordcount/counter.clj
@@ -2,37 +2,16 @@
   (:require 
     [clojure.string :as string]
     [cascalog.api :refer :all]
-    [cascalog.logic.ops :as c]
-  )
-  (:import
-    [cascading.flow.hadoop HadoopFlowProcess]
-    [cascading.operation ConcreteCall]
-  )
-)
-
-(defprepfn counterfn 
-  [^HadoopFlowProcess fp ^ConcreteCall b]
-  (fn []
-    [(mapfn 
-      ([group nam] 
-        (.increment fp group nam 1))
-      ([group nam n]
-        (.increment fp group nam n))
-     )
-    ]
-  )
-)
+    [cascalog.logic.ops :as c]))
 
 (defmapcatfn split
   "Accepts a sentence 1-tuple, splits that sentence on whitespace, and
    emits a single 1-tuple for each word."
-  [^String sentence counter]
-  (counter "wordcount" "tuples")
+  [^String sentence inc-counter]
+  (inc-counter "wordcount" "tuples")
   (let [words (string/split sentence #"\s+")]
-    (counter "wordcount" "words" (count words))
-    words
-  )
-)
+    (inc-counter "wordcount" "words" (count words))
+    words))
 
 (defn wordcount-query
   "Accepts a generator of lines of text and returns a subquery that
@@ -40,15 +19,11 @@
   [src]
   (<- [?word ?count]
       (src ?textline)
-      (counterfn :> ?counter)
-      (split ?textline ?counter :> ?word)
-      (c/count ?count)
-  )
-)
+      (counterfn :> ?inc-counter)
+      (split ?textline ?inc-counter :> ?word)
+      (c/count ?count)))
 
 ; rm -Rf output/simple && lein run -m wordcount.simple.run sample/words.txt output/simple && cat output/simple/part-00000
 (defmain run [in out]
   (?- (hfs-textline out)
-      (wordcount-query (hfs-textline in))
-  )
-)
+      (wordcount-query (hfs-textline in))))
--- src/wordcount/simple.clj
+++ src/wordcount/simple.clj
@@ -2,16 +2,13 @@
   (:require 
     [clojure.string :as string]
     [cascalog.api :refer :all]
-    [cascalog.logic.ops :as c]
-  )
-)
+    [cascalog.logic.ops :as c]))
 
 (defmapcatfn split
   "Accepts a sentence 1-tuple, splits that sentence on whitespace, and
    emits a single 1-tuple for each word."
   [^String sentence]
-  (string/split sentence #"\s+")
-)
+  (string/split sentence #"\s+"))
 
 (defn wordcount-query
   "Accepts a generator of lines of text and returns a subquery that
@@ -20,13 +17,9 @@
   (<- [?word ?count]
       (src ?textline)
       (split ?textline :> ?word)
-      (c/count ?count)
-  )
-)
+      (c/count ?count)))
 
 ; rm -Rf output/counter && lein run -m wordcount.counter.run sample/words.txt output/counter && cat output/counter/part-00000
 (defmain run [in out]
   (?- (hfs-textline out)
-      (wordcount-query (hfs-textline in))
-  )
-)
+      (wordcount-query (hfs-textline in))))
